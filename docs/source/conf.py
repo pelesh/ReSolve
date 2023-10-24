a79d00780f14a198403c18c7e70734711ebe1497
pelesh <peless@ornl.gov>
2023-10-23 21:31:19 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 import subprocess
 
-subprocess.call('$READTHEDOCS_OUTPUT/html/doxygen; cd .. ; doxygen', shell=True)
+subprocess.call('mkdir -p $READTHEDOCS_OUTPUT/html/doxygen; cd .. ; doxygen', shell=True)
 
 # -- Project information
 
